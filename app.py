f8fa5e7e395940026e1e3865a39bd48b3f6e46dd
Thiago Escobar <thaldeon@gmail.com>
2025-05-14 18:04:33 -0300

    feat: trigger action for creation of 200 tag

--- app.py
+++ app.py
@@ -5,15 +5,15 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
-    return jsonify({'error': 'Not Found'}), 404
+    return jsonify(message="Flask app is running"), 200
 
 @app.route('/health/live', methods=['GET'])
 def healthz():
-    return jsonify({'error': 'Not Found'}), 404
+    return jsonify(status="ok"), 200
 
 @app.route('/health/ready', methods=['GET'])
 def readyz():
-    return jsonify({'error': 'Not Found'}), 404
+    return jsonify(status="ready"), 200
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
