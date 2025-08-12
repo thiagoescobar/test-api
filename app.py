9a659e90d96996db947290c4211cf88b97f95039
Thiago Escobar <thaldeon@gmail.com>
2025-08-12 15:36:35 -0300

    feat: build 200 tag with latest changes

--- app.py
+++ app.py
@@ -7,15 +7,15 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
-    return '', 404
+    return '', 200
 
 @app.route('/health/live', methods=['GET'])
 def healthz():
-    return jsonify(status="ok"), 404
+    return jsonify(status="ok"), 200
 
 @app.route('/health/ready', methods=['GET'])
 def readyz():
-    return jsonify(status="ready"), 404
+    return jsonify(status="ready"), 200
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
